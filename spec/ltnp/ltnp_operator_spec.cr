14101903903adc9079b058ce1eee3946e9337be3
BraydonKains <kainsbraydon@gmail.com>
2020-03-07 23:30:04 -0500

    Using files from crystal init and reformatting everything to new editorconfig

--- spec/ltnp/ltnp_operator_spec.cr
+++ spec/ltnp/ltnp_operator_spec.cr
@@ -5,15 +5,15 @@ describe LtnpOperator do
   sut = LtnpOperator.new File.read("./spec/input/ltnp_example.txt")
 
   describe "#get_port_record" do
-	existing_port = "8081"
-	nonsense_port = "1234"
+    existing_port = "8081"
+    nonsense_port = "1234"
 
-	it "should find record when port exists" do
-	  sut.get_port_record(existing_port).nil?.should be_false
-	end
+    it "should find record when port exists" do
+      sut.get_port_record(existing_port).nil?.should be_false
+    end
 
-	it "should not find record when port doesn't exist" do
-	  sut.get_port_record(nonsense_port).nil?.should be_true
-	end
+    it "should not find record when port doesn't exist" do
+      sut.get_port_record(nonsense_port).nil?.should be_true
+    end
   end
 end
